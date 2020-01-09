52ce0615c3e16467b52367fa1866625049942224
inhibitor1217 <cytops1217@gmail.com>
2020-01-09 10:45:50 +0900

    add configuring keras backend

--- train.py
+++ train.py
@@ -8,9 +8,39 @@ from keras import backend as K
 
 from utils.config import process_config
 
+def setup_tf_session(config: DotMap):
+    tf_config = tf.ConfigProto()
+
+    if config.trainer.use_horovod:
+        import horovod.keras as hvd
+        hvd.init()
+        tf_config.gpu_options.allow_growth = True
+        tf_config.gpu_options.visible_device_list = str(hvd.local_rank())
+    
+    is_master = not config.trainer.use_horovod
+
+    if not is_master:
+        import horovod.keras as hvd
+        is_master = hvd.rank() == 0
+    
+    tf_sess = tf.Session(config=tf_config)
+    K.set_session(tf_sess)
+
+    return is_master
+
 def main(use_horovod: bool, gpus: int, config_path: str, checkpoint: int) -> None:
     config = process_config(config_path, use_horovod, gpus, checkpoint)
-    print(config)
+    
+    is_master = setup_tf_session(config)
+    
+    # copy source files
+    if is_master and not os.path.exists(config.exp.source_dir):
+        shutil.copytree(
+            os.path.abspath(os.path.curdir),
+            config.exp.source_dir,
+            ignore=lambda src, names: {'datasets', '__pycache__', '.git', 'experiments', 'venv'})
+    
+    
 
 if __name__ == '__main__':
     print(os.path.abspath(os.curdir), os.getpid())
