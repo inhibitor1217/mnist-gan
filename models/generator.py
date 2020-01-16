1aec07be66f4511721737c812ec502f05a44beab
inhibitor1217 <cytops1217@gmail.com>
2020-01-16 16:05:30 +0900

    implement deconvolutional generator

--- models/generator.py
+++ models/generator.py
@@ -1,12 +1,27 @@
-from keras.layers import Dense, Activation, Input, Reshape, Conv2D, UpSampling2D, BatchNormalization
-from keras.models import Model
+from keras import Input, Model
+from keras.layers import Conv2DTranspose, BatchNormalization
 
 from base.base_model import BaseModel
 
-
 class Generator(BaseModel):
     def define_model(self, model_name):
-        raise NotImplementedError
+        _input = Input(shape=(1, 1, 64), name=f'{model_name}_input')
+
+        x = Conv2DTranspose(filters=16, kernel_size=3, strides=4, 
+                        activation='relu', name=f'{model_name}_deconv_1')(_input)
+        x = BatchNormalization()(x)
+        x = Conv2DTranspose(filters=8, kernel_size=3, strides=2,
+                        activation='relu', name=f'{model_name}_deconv_2')(x)
+        x = BatchNormalization()(x)
+        x = Conv2DTranspose(filters=4, kernel_size=3, strides=2,
+                        activation='relu', name=f'{model_name}_deconv_3')(x)
+        x = BatchNormalization()(x)
+        x = Conv2DTranspose(filters=1, kernel_size=3, strides=2,
+                        activation='tanh', name=f'{model_name}_deconv_4')(x)
+
+        model = Model(inputs=_input, outputs=x, name=model_name)
+
+        return model
 
     def build_model(self, model_name):
         raise NotImplementedError
