c62e2882ae76289793d3726009c883fb5fc3443e
inhibitor1217 <cytops1217@gmail.com>
2020-01-16 16:24:02 +0900

    implement combined model

--- models/gan_combined.py
+++ models/gan_combined.py
@@ -1,14 +1,34 @@
-from typing import Tuple
-
 from keras import Model, Input
+from keras.layers import Concatenate
 from keras.optimizers import Adam
 
 from base.base_model import BaseModel
 from utils.layer import named
 
 class GANCombined(BaseModel):
-    def define_model(self, g: Model, d: Model, model_name: str) -> Model:
-        raise NotImplementedError
+    def define_model(self, g, d, c, model_name):
+        c.trainable = False
+        d.trainable = False
 
-    def build_model(self, g: Model, d: Model, model_name: str) -> Model:
-        raise NotImplementedError
+        _input_label = Input(shape=(1, 1, 10), name=f'{model_name}_input_label')
+        _input_noise = Input(shape=(1, 1, 54), name=f'{model_name}_input_noise')
+
+        _input = Concatenate(axis=-1, name=f'{model_name}_input')([_input_label, _input_noise])
+
+        fake = g(_input)
+
+        fake_prediction = d(fake)
+        fake_classification = c(fake)
+
+        model = Model(inputs=[_input_label, _input_noise], outputs=[fake_prediction, fake_classification], name=model_name)
+
+        return model
+
+    def build_model(self, g, d, c, model_name):
+        model = self.define_model(g, d, c, model_name)
+        optimizer = Adam(self.config.model.generator.lr)
+        parallel_model = self.multi_gpu_model(model)
+        parallel_model.compile(optimizer=optimizer, loss=['binary_crossentropy', 'binary_crossentropy'],
+                    loss_weights=[1, self.config.model.generator.weight_classifier])
+
+        return model, parallel_model
